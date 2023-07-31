810944c6236aa5e3d2fe30d36f8ea82229900506
koppai <psthu.koppai@gmail.com>
2023-07-31 18:05:35 +0630

    Create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Form from '../components/Form';
+import HerosList from '../components/HerosList';
+
+const Home = () => (
+  <>
+    <Form />
+    <HerosList />
+  </>
+);
+
+export default Home;
